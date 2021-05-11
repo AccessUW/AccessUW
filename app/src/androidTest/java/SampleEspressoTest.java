790e4b92e8be8ac821d55d09a049525bf735af44
lgtyqz <kevinlyang01@gmail.com>
2021-05-11 12:41:42 -0700

    Add instrumentation tests to Travis (hopefully)

---
+++ app/src/androidTest/java/SampleEspressoTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import models.Place;
+
+public class SampleEspressoTest {
+    @Test
+    public void starterTest() {
+        System.out.println("ESPRESSOOOOOOOO");
+        assert 2 == 2;
+    }
+}
