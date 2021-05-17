daacd86bc1c72636579e13f04ea7d2ff673ee37c
SkinnerJ99 <jskinnr132@gmail.com>
2021-05-17 11:55:40 -0700

    Added Place and Building Tests

--- app/src/test/java/PlaceTest.java
+++ app/src/test/java/PlaceTest.java
@@ -1,21 +1,46 @@
 import org.junit.Test;
+
+import java.util.HashSet;
+
+import static org.junit.Assert.*;
 import models.Place;
+import models.RouteEdge;
+
 
 public class PlaceTest {
+
+    @Test
+    public void testConstructor() {
+        Place p = new Place(1,1,true);
+        assertEquals(1, p.getX(), 0.01);
+        assertEquals(1, p.getY(), 0.01);
+        assertEquals(new HashSet<RouteEdge>(), p.getNeighbors());
+
+        Place q = new Place(2,2);
+        assertEquals(2, q.getX(), 0.01);
+        assertEquals(2, q.getY(), 0.01);
+        assertEquals(new HashSet<RouteEdge>(), q.getNeighbors());
+    }
+
     @Test
     public void testAddNeighbor() {
         Place p1 = new Place(1, 1, true);
         Place p2 = new Place(2, 2, true);
         Place p3 = new Place(3, 3, false);
-        assert p1.getNeighbors().isEmpty();
-        assert p2.getNeighbors().isEmpty();
-        assert p3.getNeighbors().isEmpty();
+        assertTrue(p1.getNeighbors().isEmpty());
+        assertTrue(p2.getNeighbors().isEmpty());
+        assertTrue(p3.getNeighbors().isEmpty());
 
         p1.addNeighbor(p2, 4, true, true);
-        assert p1.getNeighbors().size() == 1 && p2.getNeighbors().size() == 1;
+        assertEquals(1, p1.getNeighbors().size());
+        assertEquals(1, p2.getNeighbors().size());
+
         p2.addNeighbor(p1, 4, true, false);
-        assert p1.getNeighbors().size() == 1 && p2.getNeighbors().size() == 1;
+        assertEquals(1, p1.getNeighbors().size());
+        assertEquals(1, p2.getNeighbors().size());
+
         p3.addNeighbor(p1, 5, true, true);
-        assert p3.getNeighbors().size() == 1 && p1.getNeighbors().size() == 2;
+        assertEquals(2, p1.getNeighbors().size());
+        assertEquals(1, p3.getNeighbors().size());
     }
 }
